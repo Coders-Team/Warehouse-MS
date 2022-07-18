8c8d2fff2ba4f4ea00e6f242754ed9e9f9b69239
Mohammadsarairha <mohammadsarairha@gmail.com>
2022-07-18 12:52:45 +0300

    Add new .Net App

---
+++ Warehouse-MS/Warehouse-MS/Data/WarehouseDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Warehouse_MS.Data
+{
+    public class WarehouseDBContext : DbContext
+    {
+        public WarehouseDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // This calls the base method, but does nothing
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
