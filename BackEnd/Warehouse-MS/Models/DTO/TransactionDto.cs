472758721ca3eaf88fda4aa1fea8dd381a2e930c
fuad-bassam <f.man.x99@gmail.com>
2022-07-18 18:27:32 +0300

    add some service

---
+++ BackEnd/Warehouse-MS/Models/DTO/TransactionDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastMarket.Models.DTO
+{
+    public class TransactionDto
+    {
+
+        //  This is the Transaction DTO for get request 
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string OldLocation { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+
+    }
+}
---
+++ BackEnd/Warehouse-MS/Models/Services/TransactionService.cs
@@ -0,0 +1,78 @@
+﻿using FastMarket.Models.DTO;
+using FastMarket.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Warehouse_MS.Data;
+using Warehouse_MS.Models;
+
+namespace FastMarket.Models.Services
+{
+    public class TransactionService : ITransaction
+    {
+        private readonly WarehouseDBContext _context;
+
+        public TransactionService(WarehouseDBContext context)
+        {
+            _context = context;
+        }
+        // method to create new Transaction
+
+        public async Task<TransactionDto> Create(TransactionDto transactionDto)
+        {
+
+            Transaction transaction = new Transaction()
+            {
+                Type = transactionDto.Type,
+                Date = transactionDto.Date,
+                UpdateDate = transactionDto.UpdateDate,
+                OldLocation = transactionDto.OldLocation,
+                ProductId = transactionDto.ProductId,
+                UpdatedBy = transactionDto.UpdatedBy,
+            };
+            _context.Entry(transaction).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+            return transactionDto;
+        }
+
+        // method to get specific Transaction by id
+
+
+        public async Task<Transaction> GetTransaction(int id)
+        {
+            return await _context.Transaction.FirstOrDefaultAsync(z => z.Id == id);
+        }
+        // method to get all Transaction
+
+        public async Task<List<Transaction>> GetTransactions()
+        {
+            return await _context.Transaction.ToListAsync();
+        }
+        // method to update Transaction
+        public async Task<Transaction> UpdateTransaction(int id, Transaction transaction)
+        {
+            _context.Entry(transaction).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+         
+            return transaction;
+        }
+        //method to delete Transaction
+        public async Task Delete(int id)
+        {
+            Transaction transaction = await _context.Transaction.FindAsync(id);
+
+            //if (transaction == null)
+            //{
+            //    return ;
+            //}
+
+            _context.Entry(transaction).State = EntityState.Deleted;
+
+            await _context.SaveChangesAsync();
+        }
+        
+
+    }
+}
