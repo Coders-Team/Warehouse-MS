f0a55dec66ae192cb5d0f2357ecfc7575b7e26fa
fuad-bassam <f.man.x99@gmail.com>
2022-07-23 17:39:47 +0300

    m7d23

--- Warehouse-MS/Warehouse-MS/Controllers/ProductTypeController.cs
+++ Warehouse-MS/Warehouse-MS/Controllers/ProductTypeController.cs
@@ -1,12 +1,75 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Warehouse_MS.Models;
+using Warehouse_MS.Models.Interfaces;
 
 namespace Warehouse_MS.Controllers
 {
     public class ProductTypeController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductType _productType;
+
+        public ProductTypeController(IProductType productType)
         {
-            return View();
+            this._productType = productType;
         }
+
+        // GET: api/ProductTypes
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+             IEnumerable < ProductType > productType = await _productType.GetProductTypes();
+            return View(productType);
+        }
+
+        // GET: api/ProductType/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Details(int id)
+        {
+            ProductType productType = await _productType.GetProductType(id);
+            if (productType == null)
+            {
+                return View("Error");
+            }
+            return View(productType);
+        }
+
+        // PUT: api/ProductType/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(int id, ProductType productType)
+        {
+            if (id != productType.Id)
+            {
+                return View("Error");
+            }
+            ProductType newProductType = await _productType.UpdateProductType(id, productType);
+
+            return View(newProductType);
+        }
+
+        // POST: api/ProductType
+        [HttpPost]
+        public async Task<ActionResult<ProductType>> Add(ProductType productType)
+        {
+            ProductType newProductType = await _productType.Create(productType);
+            return View(newProductType);
+
+        }
+
+        // DELETE: api/ProductType/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            ProductType productType = await _productType.GetProductType(id);
+            if (productType == null)
+            {
+                return View("Error");
+            }
+            await _productType.Delete(id);
+            return Redirect("/");
+
+        }
+
     }
 }
--- Warehouse-MS/Warehouse-MS/Controllers/StorageTypeController.cs
+++ Warehouse-MS/Warehouse-MS/Controllers/StorageTypeController.cs
@@ -1,12 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Warehouse_MS.Models;
+using Warehouse_MS.Models.Interfaces;
 
 namespace Warehouse_MS.Controllers
 {
     public class StorageTypeController : Controller
     {
-        public IActionResult Index()
+        private readonly IStorageType _storageType;
+
+        public StorageTypeController(IStorageType storageType)
         {
-            return View();
+            this._storageType = storageType;
         }
+
+        // GET: api/StorageTypes
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+
+            IEnumerable<StorageType> storageTypes = await _storageType.GetStorageTypes();
+            return View(storageTypes);
+        }
+
+        // GET: api/StorageType/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            StorageType storageType = await _storageType.GetStorageType(id);
+            if (storageType == null)
+            {
+                return View("Error");
+            }
+            return View(storageType);
+        }
+
+        // PUT: api/StorageType/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(int id, StorageType storageType)
+        {
+            if (id != storageType.Id)
+            {
+                return View("Error");
+            }
+            StorageType newStorageType = await _storageType.UpdateStorageType(id, storageType);
+
+            return View(newStorageType);
+        }
+
+        // POST: api/StorageType
+        [HttpPost]
+        public async Task<ActionResult<StorageType>> Add(StorageType storageType)
+        {
+            StorageType newStorageType = await _storageType.Create(storageType);
+            return View(newStorageType);
+
+        }
+
+        // DELETE: api/StorageType/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var storgeType = await _storageType.GetStorageType(id);
+            if (storgeType == null)
+            {
+                return View("Error");
+            }
+
+            await _storageType.Delete(id);
+            return Redirect("/");
+
+        }
+
     }
 }
